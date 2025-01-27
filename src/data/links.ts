98d732a7da051d05071aa76c6d6051a71edd32c6
joannaa-r <trudopasta14@gmail.com>
2025-01-27 15:37:18 +0000

    linki

--- src/data/links.ts
+++ src/data/links.ts
@@ -5,13 +5,9 @@ import GithubIcon from '@/components/icons/GithubIcon'
 export const SOCIALNETWORKS = [
 	{
 		name: 'Github',
-		url: 'https://github.com/danielcgilibert/blog-template',
+		url: 'https://github.com/',
 		icon: GithubIcon
 	},
 
-	{
-		name: 'Twitter',
-		url: 'https://github.com/danielcgilibert/blog-template',
-		icon: TwitterIcon
-	}
+	
 ] as const
